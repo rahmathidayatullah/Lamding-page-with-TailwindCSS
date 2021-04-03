49a338849c16d5f7ef14ca1352ab49d95744e31c
rahmathidayatullah <rahmathidayatullah996@gmail.com>
2021-04-03 12:52:55 +0700

    landing page tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,44 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      spacing: {
+        224: "224px",
+        316: "316px",
+        38: "38px",
+      },
+      boxShadow: {
+        card: "0px 4px 13px rgba(0, 0, 0, 0.1)",
+      },
+      borderRadius: {
+        10: "10px",
+      },
+      textColor: {
+        purple: "#7A47FF",
+        red: "#FD4323",
+      },
+      gradientColorStops: {
+        orange: "#F99500",
+        purple1: "#6C63FF",
+        purple2: "#675FF4",
+      },
+      fontSize: {
+        22: "22px",
+      },
+      textColor: {
+        purple: "#6860F5",
+      },
+      borderColor: {
+        purple: "#6860F5",
+      },
+      backgroundColor: {
+        purple: "#6860F5",
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
